a080098c80abe11cbd9632ce8a01fedd330cc432
Poloju Sai Chaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2026-01-23 20:41:15 +0530

    Time: 26 ms (38.31%) | Memory: 60.3 MB (91.74%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.java
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -1,41 +1,38 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        //hello
-        int n=graph.length;
+        int n = graph.length;
 
-        ArrayList<ArrayList<Integer>> graphRev=new ArrayList<>();
-        for(int i=0;i<n;i++)
-        graphRev.add(new ArrayList<>());
+        ArrayList<ArrayList<Integer>> graphRev = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            graphRev.add(new ArrayList<>());
 
-        int[] inDeg=new int[n];
+        int[] inDeg = new int[n];
 
-        for(int i=0;i<n;i++){
-            for(int it : graph[i]){
+        for (int i = 0; i < n; i++) {
+            for (int it : graph[i]) {
                 graphRev.get(it).add(i);
                 inDeg[i]++;
             }
         }
 
-        Queue<Integer> q=new LinkedList<>();
-        List<Integer> ans=new ArrayList<>();
+        Queue<Integer> q = new LinkedList<>();
+        List<Integer> ans = new ArrayList<>();
 
-        for(int i=0;i<n;i++){
-            if(inDeg[i] == 0)
-            q.add(i);
+        for (int i = 0; i < n; i++) {
+            if (inDeg[i] == 0)
+                q.add(i);
         }
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int node = q.poll();
             ans.add(node);
 
-            for(int adjacent : graphRev.get(node) ){
+            for (int adjacent : graphRev.get(node)) {
                 inDeg[adjacent]--;
-                if(inDeg[adjacent] == 0)
-                q.add(adjacent);
+                if (inDeg[adjacent] == 0)
+                    q.add(adjacent);
             }
         }
-
-        System.out.println(ans);
 
         Collections.sort(ans);
 
