7164ba5b6d96d5db53c402ac412ff80577916b3a
Poloju Sai Chaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2026-01-23 20:37:30 +0530

    Time: 33 ms (13.64%) | Memory: 65.5 MB (10.24%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        //hello
+        int n=graph.length;
+
+        ArrayList<ArrayList<Integer>> graphRev=new ArrayList<>();
+        for(int i=0;i<n;i++)
+        graphRev.add(new ArrayList<>());
+
+        int[] inDeg=new int[n];
+
+        for(int i=0;i<n;i++){
+            for(int it : graph[i]){
+                graphRev.get(it).add(i);
+                inDeg[i]++;
+            }
+        }
+
+        Queue<Integer> q=new LinkedList<>();
+        List<Integer> ans=new ArrayList<>();
+
+        for(int i=0;i<n;i++){
+            if(inDeg[i] == 0)
+            q.add(i);
+        }
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            ans.add(node);
+
+            for(int adjacent : graphRev.get(node) ){
+                inDeg[adjacent]--;
+                if(inDeg[adjacent] == 0)
+                q.add(adjacent);
+            }
+        }
+
+        System.out.println(ans);
+
+        Collections.sort(ans);
+
+        return ans;
+    }
+}
