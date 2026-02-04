941fb1917254cee96e3543d0ee77d81f894d5a41
Poloju Sai Chaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2026-02-04 21:18:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (26.91%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans=new ArrayList<>();
+
+        // Root->Right->Left
+        revPreOrder(root,0,ans);
+
+        return ans;
+    }
+    void revPreOrder(TreeNode root,int level,List<Integer> ans){
+        if(root == null) return;
+
+        if(level == ans.size())
+        ans.add(root.val);
+
+        revPreOrder(root.right,level+1,ans);
+        revPreOrder(root.left,level+1,ans);
+    }
+}
