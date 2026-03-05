25393b4f9946ff9a6de3fbc62feae8a1e149c75a
Poloju Sai Chaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2026-03-05 20:24:11 +0530

    Time: 3 ms (73.32%) | Memory: 47.2 MB (83.87%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,92 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ class BSTIteratorNext {
+
+    Stack<TreeNode> stk;
+
+    public BSTIteratorNext(TreeNode root) {
+        stk = new Stack<>();
+        goLeft(root);
+    }
+
+    public void goLeft(TreeNode root) {
+        while (root != null) {
+            stk.push(root);
+            root = root.left;
+        }
+    }
+
+    public int next() {
+        TreeNode node = null;
+        if (!stk.isEmpty()) {
+            node = stk.pop();
+            if (node.right != null)
+                goLeft(node.right);
+        }
+        return node.val;
+    }
+
+}
+
+ class BSTIteratorBefore {
+
+    Stack<TreeNode> stk;
+
+    public BSTIteratorBefore(TreeNode root) {
+        stk = new Stack<>();
+        goRight(root);
+    }
+
+    public void goRight(TreeNode root) {
+        while (root != null) {
+            stk.push(root);
+            root = root.right;
+        }
+    }
+
+    public int before() {
+        TreeNode node = null;
+        if (!stk.isEmpty()) {
+            node = stk.pop();
+            if (node.left != null)
+                goRight(node.left);
+        }
+        return node.val;
+    }
+
+}
+class Solution {
+    public boolean findTarget(TreeNode root, int k) {
+        BSTIteratorNext nextObj=new BSTIteratorNext(root);
+        BSTIteratorBefore beforeObj=new BSTIteratorBefore(root);
+
+            int i = nextObj.next();
+            int j=beforeObj.before();
+
+        while(i<j){
+            if(i+j > k)
+            j=beforeObj.before();
+            else if(i+j < k)
+            i=nextObj.next();
+            else
+            return true;
+        }
+
+        return false;
+
+    }
+}
