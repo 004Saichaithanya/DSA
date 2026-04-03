b927b7d0370f64a0169b5ed1c344323cc389b976
Poloju Sai Chaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2026-04-03 11:43:53 +0530

    Time: 67 ms (66.61%) | Memory: 76.2 MB (93.28%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,53 +1,30 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        int maxArea=0;
+        Stack<Integer> stk=new Stack<>();
+        int n=heights.length;
 
-        int[] nse = nseFunc(heights);
-        int[] pse = pseFunc(heights);
+        for(int i=0;i<n;i++){
+            while(!stk.isEmpty() && heights[i] <= heights[stk.peek()]){
+                int ele= stk.peek();
+                stk.pop();
+                int pse = stk.isEmpty()?-1:stk.peek();
+                int nse = i;
+                maxArea  = Math.max(maxArea, heights[ele] * (nse - pse - 1));
+            }
+            stk.push(i);
+        }
 
-        int maxArea = 0;
+        while(!stk.isEmpty()){
+            int nse = n;
+            int element = stk.peek();
+            stk.pop();
+            int pse = stk.isEmpty()?-1:stk.peek();
 
-        for (int i = 0; i < heights.length; i++) {
-            maxArea = Math.max(maxArea, heights[i] * (nse[i] - pse[i] - 1));
+            maxArea = Math.max(maxArea , heights[element] * (nse - pse - 1));
+
         }
 
         return maxArea;
-
     }
-
-    int[] nseFunc(int[] heights) {
-        Stack<Integer> stk = new Stack<>();
-        int n = heights.length;
-        int[] nse = new int[n];
-
-        for (int i = n - 1; i >= 0; i--) {
-            while (!stk.isEmpty() && heights[i] <= heights[stk.peek()]) {
-                stk.pop();
-            }
-            nse[i] = stk.isEmpty() ? n : stk.peek();
-
-            stk.push(i);
-        }
-
-        return nse;
-    }
-
-    int[] pseFunc(int[] heights) {
-        Stack<Integer> stk = new Stack<>();
-        int n = heights.length;
-        int[] pse = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            while (!stk.isEmpty() && heights[i] <= heights[stk.peek()]) {
-                stk.pop();
-            }
-
-            pse[i] = stk.isEmpty() ? -1 : stk.peek();
-
-            stk.push(i);
-        }
-
-        return pse;
-
-    }
-
 }
