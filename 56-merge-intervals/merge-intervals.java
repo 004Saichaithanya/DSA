c312c6229ecc3066cdc7bf08ace574871e05fb45
Poloju Sai Chaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2026-04-04 17:44:39 +0530

    Time: 7 ms (98.91%) | Memory: 49.5 MB (8.55%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,36 +1,27 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
-        ArrayList<int[]> res = new ArrayList<>();
 
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
-        //         for (int[] in : intervals) {
-        // System.out.println("[" + in[0] + "," + in[1] + "]");
-        // }
-
-        res.add(new int[] { intervals[0][0], intervals[0][1] });
+        int idx = 0; // points to last merged interval
 
         for (int i = 1; i < n; i++) {
-            int[] last = res.getLast();
-            if (intervals[i][0] <= last[1]) {
-                int cur_end = intervals[i][1];
-                last[1] = Math.max(cur_end, last[1]);
+            // overlap
+            if (intervals[i][0] <= intervals[idx][1]) {
+                intervals[idx][1] = Math.max(intervals[idx][1], intervals[i][1]);
             } else {
-                res.add(new int[] { intervals[i][0], intervals[i][1] });
+                // no overlap → move forward
+                idx++;
+                intervals[idx] = intervals[i];
             }
-
         }
 
-        // for(int[] in : res){
-        //   //  System.out.println("hi");
-        //     System.out.println("[" + in[0] + "," + in[1] + "]");
-        // }
-
-        int[][] ans = new int[res.size()][];
-
-        for (int i = 0; i < ans.length; i++)
-            ans[i] = res.get(i);
+        // create result array of size idx+1
+        int[][] ans = new int[idx + 1][];
+        for (int i = 0; i <= idx; i++) {
+            ans[i] = intervals[i];
+        }
 
         return ans;
     }
