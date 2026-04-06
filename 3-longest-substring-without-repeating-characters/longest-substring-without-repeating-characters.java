4a124587fbc1e5e7d12231cb50ad52f68b583a88
Poloju Sai Chaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2026-04-06 20:26:28 +0530

    Time: 8 ms (17.93%) | Memory: 45.5 MB (79.28%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,24 +1,20 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int start=0,end=0;
-        int n=s.length();
-        HashMap<Character,Integer> map=new HashMap<>();
-        int maxLen=0;
+        int n = s.length();
+        int start = 0, end = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int maxLen = 0;
 
-        while(end < n){
-            map.put(s.charAt(end),map.getOrDefault(s.charAt(end),0)+1);
-
-            if(map.get(s.charAt(end)) > 1){
-                while(map.get(s.charAt(end)) > 1 ){
-                    map.put(s.charAt(start),map.get(s.charAt(start))-1);
+        while (end < n) {
+            map.put(s.charAt(end), map.getOrDefault(s.charAt(end), 0) + 1);
+            if (map.get(s.charAt(end)) > 1) {
+                while (map.get(s.charAt(end)) > 1) {
+                    map.put(s.charAt(start), map.get(s.charAt(start)) - 1);
                     start++;
                 }
             }
-            int len = end - start + 1;
-            maxLen=Math.max(len,maxLen);
-
+            maxLen = Math.max(maxLen, end - start + 1);
             end++;
-
         }
 
         return maxLen;
