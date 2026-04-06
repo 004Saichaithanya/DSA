d01e14a5074e631f22cbe209dba580c18ce14ec2
Poloju Sai Chaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2026-04-06 21:20:33 +0530

    Time: 17 ms (26.16%) | Memory: 49.4 MB (94.01%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> map = new HashMap<>();
+        int n = strs.length;
+
+        for (int i = 0; i < n; i++) {
+            String temp = strs[i];
+            String key = isAnagram(temp);
+
+            if (!map.containsKey(key))
+                map.put(key, new ArrayList<>(Arrays.asList(temp)));
+            else
+                map.get(key).add(temp);
+        }
+        // for(List<String> temp : map.values()){
+        //     for(String x : temp)
+        //         System.out.print(x+" ");
+        //     System.out.println();
+        // }
+
+        return new ArrayList<>(map.values());
+    }
+
+    public String isAnagram(String s) {
+
+        int[] hash = new int[26];
+        StringBuilder res = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            hash[ch - 'a']++;
+        }
+
+        for (int x : hash) {
+            res.append(x).append('#');
+        }
+
+        return res.toString();
+
+    }
+}
