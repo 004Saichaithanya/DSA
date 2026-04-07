c9ec36131294c64bcac1f3ba900fe831d8fe45f8
Poloju Sai Chaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2026-04-07 21:00:11 +0530

    Time: 1 ms (100.00%) | Memory: 43.9 MB (31.67%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int myAtoi(String s) {
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        if (s.length() == 0)
+            return 0;
+        s = s.trim();
+        int n = s.length();
+        if (s.isEmpty())
+            return 0;
+
+        //  int isNumber=0;
+        int sign = 1;
+        //int other=0;
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+
+        if (s.charAt(0) == '-' || s.charAt(0) == '+') {
+            if (s.charAt(0) == '-')
+                sign = -1;
+            i = 1;
+        }
+
+        //leading zeros
+        while (i < n && s.charAt(i) == 0) {
+            i++;
+        }
+
+        int ans = 0;
+
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+
+            //overflow condtion
+            if (ans > (max - digit) / 10)
+                return (sign == -1) ? min : max;
+
+            ans = ans * 10 + digit;
+            i++;
+        }
+
+        return sign * ans;
+    }
+}
