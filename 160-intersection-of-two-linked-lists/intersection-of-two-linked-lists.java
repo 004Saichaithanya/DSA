7199f6fbf348b39a3b0d4eea37ee683494a6ab74
Poloju Sai Chaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2026-04-14 10:53:31 +0530

    Time: 2 ms (52.06%) | Memory: 53 MB (16.68%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,21 +11,44 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA == null || headB == null) return null;
-        ListNode t1=headA;
-        ListNode t2=headB;
-        Set<ListNode> set=new HashSet<>();
+        if (headA == null || headB == null)
+            return null;
+        ListNode t1 = headA;
+        ListNode t2 = headB;
+        int Alen = 0, Blen = 0;
 
-        while(t1 != null){
-            set.add(t1);
-            t1=t1.next;
+        while (t1 != null) {
+            Alen++;
+            t1 = t1.next;
         }
-        while(t2 != null){
-            if(set.contains(t2))
-                return t2;
-            t2=t2.next;
+
+        while (t2 != null) {
+            Blen++;
+            t2 = t2.next;
+        }
+        int steps = Math.abs(Alen - Blen);
+
+        if (Alen >= Blen)
+            return collisionPoint(headA, headB, steps);
+        else if (Blen > Alen)
+            return collisionPoint(headB, headA, steps);
+
+        return null;
+    }
+
+    ListNode collisionPoint(ListNode large, ListNode small, int steps) {
+        while (steps > 0) {
+            large = large.next;
+            steps--;
+        }
+        while (large != null && small != null) {
+            if (large == small)
+                return large;
+            large = large.next;
+            small = small.next;
         }
 
         return null;
     }
+
 }
