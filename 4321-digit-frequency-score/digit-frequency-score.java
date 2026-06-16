66c463cc688d8019c29b972e1b1538a3e24cdf3e
Poloju Sai Chaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2026-06-16 23:36:05 +0530

    Time: 3 ms (20.67%) | Memory: 42.6 MB (61.39%) - LeetSync

--- 4321-digit-frequency-score/digit-frequency-score.java
+++ 4321-digit-frequency-score/digit-frequency-score.java
@@ -1,11 +1,18 @@
 class Solution {
     public int digitFrequencyScore(int n) {
+        HashMap<Character,Integer> map=new HashMap<>();
+        String number  = Integer.toString(n);
 
-    int sum = 0;
-    String temp = n+"";
-    for(char ch : temp.toCharArray()){
-        sum += ch - '0';
-    }
-    return sum;
+        for(char ch : number.toCharArray()){
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+
+        int sum = 0;
+
+        for(char key : map.keySet()){
+            sum += (key - '0') * (map.get(key));
+        }
+
+        return sum;
     }
 }
