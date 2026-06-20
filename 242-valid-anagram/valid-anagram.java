13f87f1d09412a35601a9dfa638510a0e05f2f53
Poloju Sai Chaithanya <128970858+004Saichaithanya@users.noreply.github.com>
2026-06-20 17:53:20 +0530

    Time: 2 ms (99.59%) | Memory: 44.7 MB (56.57%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,23 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length())
-            return false;
-        int[] hash = new int[26];
+        int[] hash=new int[26];
 
-        for (char ch : s.toCharArray()) {
+        for(char ch : s.toCharArray()){
             hash[ch - 'a']++;
         }
-
-        for (char ch : t.toCharArray()) {
+        for(char ch : t.toCharArray())
             hash[ch - 'a']--;
-        }
 
-        for (int x : hash) {
-            if (x != 0)
+        for(int x : hash){
+            if(x != 0)
                 return false;
         }
 
         return true;
-
     }
 }
